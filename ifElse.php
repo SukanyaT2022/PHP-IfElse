16df696685b4af7ae98be94d910a6c731e63200c
TiparpronS <Sukanyat2022a@gmail.com>
2023-01-13 10:28:43 -0500

    first

---
+++ ifElse.php
@@ -0,0 +1,36 @@
+<?php
+//asssign one
+//Write a php code to create two variables called num1 and num2. Store value 20 in num1 and 25 in num2. 
+//Check which amongst the two is greater and print
+// $num1 = 20;
+// $num2 = 25;
+// if ($num1 > $num2){
+//     echo "false num1 less than num2";
+// }
+// else if ($num1 < $num2){
+//     echo "true num2 greater than num1";
+//     }
+//     else{
+//         echo "nothing";
+//     }
+
+//assign two
+
+//Write a php code to create a variable with some value.
+// Check if the variable is positive, negative or zero
+
+$num = 0;
+if($num > 0){
+    echo "positive";
+}
+
+else if($num < 0){
+    echo "negative";
+}
+
+else{
+    echo "zero";
+}
+
+
+?>
